9ab688beaf2465bf8441a513781fe71e01d4f768
Jose Espinoza <espinozajgx@gmail.com>
2023-07-09 12:38:36 +0200

    feat(quarkus) added custom properties

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -4,15 +4,19 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.Arrays;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name = "greeting.message")
+    String message;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "{" + message + "} from RESTEasy Reactive";
     }
 }
