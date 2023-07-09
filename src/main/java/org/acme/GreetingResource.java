531b5a5ef1680a40c98e139aa4d44d1d005235a4
Jose Espinoza <espinozajgx@gmail.com>
2023-07-09 12:50:43 +0200

    feat(quarkus) read properties and yaml variables

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,15 +1,20 @@
 package org.acme;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.Arrays;
 
 @Path("/hello")
 public class GreetingResource {
+
+    @Inject
+    Config config;
 
     @ConfigProperty(name = "greeting.message")
     String message;
@@ -18,5 +23,13 @@ public class GreetingResource {
     @Produces(MediaType.APPLICATION_JSON)
     public String hello() {
         return "{" + message + "} from RESTEasy Reactive";
+    }
+
+    @GET
+    @Path("/config")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloConfig() {
+        config.getPropertyNames().forEach( p -> System.out.println(p));
+        return config.getValue("greeting.message", String.class);
     }
 }
