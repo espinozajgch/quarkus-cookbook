d1ba3708290af22208c40b61374bb6de4e13cc5d
Jose Espinoza <espinozajgx@gmail.com>
2023-07-09 11:54:00 +0200

    feat(quarkus) greeting

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,9 +1,10 @@
 package org.acme;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
 import java.util.Arrays;
 
 @Path("/hello")
@@ -11,14 +12,7 @@ public class GreetingResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String
-    hello() {
-
-        int[] a = {1,2,3};
-        String[] b = new String[5];
-        int[] c = new int[10];
-        Arrays.asList("1","2",3);
-
+    public String hello() {
         return "Hello from RESTEasy Reactive";
     }
 }
