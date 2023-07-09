2dff106555f8cc9f8fced52e2a46573c0aab933d
Jose Espinoza <espinozajgx@gmail.com>
2023-07-09 14:03:30 +0200

    feat(quarkus) loggin configs whit profiles

---
+++ src/main/java/org/acme/log/GelfLoggingResource.java
@@ -0,0 +1,19 @@
+package org.acme.log;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import org.jboss.logging.Logger;
+
+@Path("/gelf-logging")
+@ApplicationScoped
+public class GelfLoggingResource {
+    private static final Logger LOG = Logger.getLogger(GelfLoggingResource.class);
+
+    @GET
+    public void log() {
+        LOG.info("Some useful log message");
+    }
+
+}
